a29660d4878f5ec1583f244a1af6b8bd07e72ad4
poocong <81405011+poocong@users.noreply.github.com>
2021-04-16 19:20:40 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -45,13 +45,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("Mendonwnload musik by @Hyoneechan...")
+    m.edit("Mendonwnload musik by @Pocongonlen...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Mengupload musik by @Hyoneechan**'
+        rep = '**🎵 Mengupload musik by @Pocongonlen**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
